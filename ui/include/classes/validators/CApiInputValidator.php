01a75626d306d67d3c62ddbe234ad6c002ee8bf2
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2021-11-09 22:32:38 +0200

    .......... [DEV-2003] fixed jenkins tests3

--- ui/include/classes/validators/CApiInputValidator.php
+++ ui/include/classes/validators/CApiInputValidator.php
@@ -1764,7 +1764,7 @@ class CApiInputValidator {
 	 * @return bool
 	 */
 	private static function validateRangeTime($rule, &$data, $path, &$error) {
-		array_key_exists('flags', $rule) ? $rule['flags'] : 0x00;
+		$flags = array_key_exists('flags', $rule) ? $rule['flags'] : 0x00;
 
 		if (self::checkStringUtf8(API_NOT_EMPTY, $data, $path, $error) === false) {
 			return false;
