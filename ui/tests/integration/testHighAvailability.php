359b9ac34c4e849970271458091b15e9b543a211
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-02-11 03:05:22 +0200

    .......... [DEV-2000] xxx

--- ui/tests/integration/testHighAvailability.php
+++ ui/tests/integration/testHighAvailability.php
@@ -131,13 +131,10 @@ class testHighAvailability extends CIntegrationTest {
 	 * @configurationDataProvider serverConfigurationProvider_ha
 	 */
 	public function testHighAvailability_checkModeSwitching2() {
-		$this->startComponent(self::COMPONENT_SERVER_HANODE1);
-		//$this->assertTrue($this->waitForLogLineToBePresent(self::COMPONENT_SERVER_HANODE1, '"'.self::NODE2_NAME.'" node started in "standby" mode', true, 5, 15));
-
 		$this->stopComponent(self::COMPONENT_SERVER);
 		$this->startComponent(self::COMPONENT_SERVER, "HA manager started");
 
-		$this->executeRuntimeControlCommand(self::COMPONENT_SERVER, 'ha_status');
+		$this->executeRuntimeControlCommand(self::COMPONENT_SERVER_HANODE1, 'ha_status');
 		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER_HANODE1, 'sss', true, 1, 1);
 		$this->stopComponent(self::COMPONENT_SERVER_HANODE1);
 
