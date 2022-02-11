5ac2a2ed2bec5b9dc9a2f252e6d52ececa8feca0
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-02-11 16:07:46 +0200

    .......... [DEV-2000] xxx

--- ui/tests/integration/testHighAvailability.php
+++ ui/tests/integration/testHighAvailability.php
@@ -202,10 +202,8 @@ class testHighAvailability extends CIntegrationTest {
 	public function testHighAvailability_failover() {
 		$this->stopComponent(self::COMPONENT_SERVER);
 		$this->startComponent(self::COMPONENT_SERVER, 'HA manager started');
-		$this->executeRuntimeControlCommand(self::COMPONENT_SERVER_HANODE1, 'ha_set_failover_delay=10s');
 		$this->executeRuntimeControlCommand(self::COMPONENT_SERVER, 'ha_set_failover_delay=10s');
-		//$this->waitForLogLineToBePresent(self::COMPONENT_SERVER_HANODE1, 'HA failover delay set to 10s', true, 20, 3);
-
+		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER, 'HA failover delay set to 10s', true, 20, 3);
 
 		$ha_mgr_pid = shell_exec("ps -eo pid,cmd | grep -E 'server_ha1: ha manager' | grep -v grep | grep -Eo '\b[0-9]+\b'");
 		posix_kill(intval($ha_mgr_pid), 9);
