f3215cc9fc6c8ff0561385ab495790c567ea6696
Dmitrijs Goloscapovs <dmitrijs.goloscapovs@zabbix.com>
2022-02-11 17:12:11 +0200

    .......... [DEV-2000] xxx

--- ui/tests/integration/testHighAvailability.php
+++ ui/tests/integration/testHighAvailability.php
@@ -211,7 +211,7 @@ class testHighAvailability extends CIntegrationTest {
 		$ha_srv_pid = file_get_contents(self::getPidPath(self::COMPONENT_SERVER_HANODE1));
 		posix_kill(intval($ha_srv_pid), 9);
 
-		sleep(12);
+		sleep(30);
 
 		$response = $this->call('hanode.get', [
 			'output' => 'extend',
