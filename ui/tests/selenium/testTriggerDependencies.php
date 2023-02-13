2fc3e2fe7bfd9640a4370a0803544663679b14d0
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2023-02-13 14:26:45 +0200

    .......... [DEV-2395] added create scenario

--- ui/tests/selenium/testTriggerDependencies.php
+++ ui/tests/selenium/testTriggerDependencies.php
@@ -36,6 +36,19 @@ class testTriggerDependencies extends CWebTest {
 	protected static $trigger_protids;
 	protected static $hostids;
 	protected static $host_itemids;
+	protected static $host_triggerids;
+	protected static $host_druleids;
+	protected static $host_item_protids;
+	protected static $host_trigger_protids;
+
+	/**
+	 * Attach Behaviors to the test.
+	 *
+	 * @return array
+	 */
+	public function getBehaviors() {
+		return ['class' => CMessageBehavior::class];
+	}
 
 	public function prepareTriggersData() {
 		$templates = CDataHelper::call('template.create', [
@@ -86,11 +99,11 @@ class testTriggerDependencies extends CWebTest {
 				'expression' => 'last(/Template with everything/everything)=0'
 			],
 			[
-				'description' => 'trigger linked 1',
+				'description' => 'trigger linked',
 				'expression' => 'last(/Template that linked to host/everything_2)=0'
 			],
 			[
-				'description' => 'trigger linked 2',
+				'description' => 'trigger linked update',
 				'expression' => 'last(/Template that linked to host/everything_2)=0'
 			]
 		]);
@@ -149,11 +162,11 @@ class testTriggerDependencies extends CWebTest {
 				'expression' => 'last(/Template with everything/everything_prot_[{#KEY}])=0'
 			],
 			[
-				'description' => 'trigger prototype linked 1{#KEY}',
+				'description' => 'trigger prototype linked{#KEY}',
 				'expression' => 'last(/Template that linked to host/linking_prot_[{#KEY}])=0'
 			],
 			[
-				'description' => 'trigger prototype linked 2{#KEY}',
+				'description' => 'trigger prototype linked update{#KEY}',
 				'expression' => 'last(/Template that linked to host/linking_prot_[{#KEY}])=0'
 			]
 		]);
@@ -162,7 +175,7 @@ class testTriggerDependencies extends CWebTest {
 
 		$hosts = CDataHelper::call('host.create', [
 			[
-				'host' => 'With linked template',
+				'host' => 'Host with linked template',
 				'templates' => [
 					'templateid' => self::$templateids['Template that linked to host']
 				],
@@ -171,7 +184,7 @@ class testTriggerDependencies extends CWebTest {
 				]
 			],
 			[
-				'host' => 'With everything',
+				'host' => 'Host with everything',
 				'groups' => [
 					['groupid' => 4]
 				]
@@ -184,7 +197,7 @@ class testTriggerDependencies extends CWebTest {
 			[
 				'name' => 'Host item 1',
 				'key_' => 'host_item_1',
-				'hostid' => self::$hostids['With everything'],
+				'hostid' => self::$hostids['Host with everything'],
 				'type' => 2,
 				'value_type' => 3,
 				'delay' => 0
@@ -192,7 +205,7 @@ class testTriggerDependencies extends CWebTest {
 			[
 				'name' => 'Host item 2',
 				'key_' => 'host_item_2',
-				'hostid' => self::$hostids['With everything'],
+				'hostid' => self::$hostids['Host with linked template'],
 				'type' => 2,
 				'value_type' => 3,
 				'delay' => 0
@@ -201,10 +214,117 @@ class testTriggerDependencies extends CWebTest {
 		$this->assertArrayHasKey('itemids', $host_items);
 		self::$host_itemids = CDataHelper::getIds('name');
 
-		$host_triggers
+		$host_triggers = CDataHelper::call('trigger.create', [
+			[
+				'description' => 'Host trigger update',
+				'expression' => 'last(/Host with everything/host_item_1)=0'
+			],
+			[
+				'description' => 'Host trigger everything',
+				'expression' => 'last(/Host with everything/host_item_1)=0'
+			],
+			[
+				'description' => 'Host trigger 2',
+				'expression' => 'last(/Host with linked template/host_item_2)=0'
+			]
+		]);
+		$this->assertArrayHasKey('triggerids', $host_triggers);
+		self::$host_triggerids = CDataHelper::getIds('description');
+
+		$host_drule = CDataHelper::call('discoveryrule.create', [
+			[
+				'name' => 'Drule for host everything',
+				'key_' => 'host_everything_drule',
+				'hostid' => self::$hostids['Host with everything'],
+				'type' => 2,
+				'delay' => 0
+			],
+			[
+				'name' => 'Drule for host with linking',
+				'key_' => 'host_linked_drule',
+				'hostid' => self::$hostids['Host with linked template'],
+				'type' => 2,
+				'delay' => 0
+			]
+		]);
+		$this->assertArrayHasKey('itemids', $host_drule);
+		self::$host_druleids = CDataHelper::getIds('name');
+
+		$host_item_prot = CDataHelper::call('itemprototype.create', [
+			[
+				'name' => 'Host Item prot with everything',
+				'key_' => 'host_everything_prot_[{#KEY}]',
+				'hostid' => self::$hostids['Host with everything'],
+				'ruleid' => self::$host_druleids['Drule for host everything'],
+				'type' => 2,
+				'value_type' => 3,
+				'delay' => 0
+			],
+			[
+				'name' => 'Host Item prot for linking',
+				'key_' => 'host_linking_prot_[{#KEY}]',
+				'hostid' => self::$hostids['Host with linked template'],
+				'ruleid' => self::$host_druleids['Drule for host with linking'],
+				'type' => 2,
+				'value_type' => 3,
+				'delay' => 0
+			]
+		]);
+		$this->assertArrayHasKey('itemids', $host_item_prot);
+		self::$host_item_protids = CDataHelper::getIds('name');
+
+		$host_trigger_prot = CDataHelper::call('triggerprototype.create', [
+			[
+				'description' => 'Host trigger prot update{#KEY}',
+				'expression' => 'last(/Host with everything/host_everything_prot_[{#KEY}])=0'
+			],
+			[
+				'description' => 'Host trigger prot simple{#KEY}',
+				'expression' => 'last(/Host with everything/host_everything_prot_[{#KEY}])=0'
+			],
+			[
+				'description' => 'Host trigger prot for linked{#KEY}',
+				'expression' => 'last(/Host with linked template/host_linking_prot_[{#KEY}])=0'
+			],
+			[
+				'description' => 'Host trigger prot for linked update{#KEY}',
+				'expression' => 'last(/Host with linked template/host_linking_prot_[{#KEY}])=0'
+			]
+		]);
+		$this->assertArrayHasKey('triggerids', $host_trigger_prot);
+		self::$host_trigger_protids = CDataHelper::getIds('description');
+
 	}
 
+	public static function getHostCreateData() {
+		return [
+
+		];
+	}
+
+
+
+
+	/**
+	 * Create trigger with dependencies on host.
+	 *
+	 * @dataProvider getHostCreateData
+	 */
 	public function testTriggerDependencies_Create() {
-//		var_dump(self::$templateids['Template 1']);
+		$this->page->login()->open('triggers.php?hostid=99262&form=create&context=host')->waitUntilReady();
+		$form = $this->query('name:triggersForm')->asForm()->one();
+		$form->fill(['Name' => 'test', 'Expression' => 'last(/Host with everything/host_item_1)=0']);
+		$form->selectTab('Dependencies')->waitUntilReady();
+		$form->query('id:add_dep_trigger')->one()->click();
+		$dialog = COverlayDialogElement::find()->one()->waitUntilReady();
+		$dialog->query('id:generic-popup-form')->asMultiselect()->one()->fill(['Host' => 'Host with everything']);
+		$dialog->waitUntilReady();
+
+		$dialog->query('xpath:.//input[@value="100215"]')->asCheckbox()->one()->check();
+		$dialog->query('xpath:(.//button[text()="Select"])[2]')->one()->click();
+		$dialog->waitUntilNotVisible();
+
+		$form->submit();
+		$this->assertMessage(TEST_GOOD, 'Trigger added');
 	}
 }
